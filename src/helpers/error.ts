beb1496cc15090bffda2f2ebbb6f6dd22f49cb7b
xuyufan6 <1067431030@qq.com>
2019-07-29 00:05:26 +0800

    feat: 增加error对象，
    增加xhr, data,header, url ,error处理对象函数
    
    BREAKING CHANGE: n

---
+++ src/helpers/error.ts
@@ -0,0 +1,40 @@
+import { AxiosRequestConfig, AxiosResponse } from '../types'
+
+export class AxiosError extends Error {
+  isAxiosError: boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+
+  constructor(
+    message: string,
+    config: AxiosRequestConfig,
+    code?: string | null,
+    request?: any,
+    response?: AxiosResponse
+  ) {
+    super(message)
+
+    this.config = config
+    this.code = code
+    this.request = request
+    this.response = response
+
+    this.isAxiosError = true
+
+    // 解决ts内置对象的问题： 不加上下面这句，是使用不到该继承内置对象的方法
+    Object.setPrototypeOf(this, AxiosError.prototype)
+  }
+}
+
+export function createError(
+  message: string,
+  config: AxiosRequestConfig,
+  code?: string | null,
+  request?: any,
+  response?: AxiosResponse
+) {
+  const error = new AxiosError(message, config, code, request, response)
+  return error
+}
---
+++ src/helpers/url.ts
@@ -0,0 +1,59 @@
+import { isDate, isPlainObject } from './utils'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/gi, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/gi, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+
+    let values = []
+
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+
+    values.forEach(v => {
+      if (isDate(v)) {
+        v = v.toISOString()
+      }
+      if (isPlainObject(v)) {
+        v = JSON.stringify(v)
+      }
+      parts.push(`${encode(key)}=${encode(v)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+
+  return url
+}
